be4e3d01290811440ab56823b2accf5e528805bd
yoonisss <kyong4962@naver.com>
2023-03-31 16:58:02 +0900

    0331 16:57

---
+++ walking_Team/src/main/java/com/example/walking/model/Photo.kt
@@ -0,0 +1,10 @@
+package com.example.walking.model
+
+import android.net.Uri
+import java.util.Date
+
+data class Photo(
+    var hashtag: String,
+    var photo_board_date: String,
+    var mainUri: String
+)
---
+++ walking_Team/src/main/java/com/example/walking/model/PhotoImg.kt
@@ -0,0 +1,5 @@
+package com.example.walking.model
+
+data class PhotoImg(
+    var ImgUrl: String
+)
